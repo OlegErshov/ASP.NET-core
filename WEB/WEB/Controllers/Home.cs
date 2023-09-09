dd6ccf115e0bd419f7a0864f0bfc7537769f69ce
olezh <olezhka1629@gmail.com>
2023-09-09 22:29:42 +0300

    first lab version 0.9.5

---
+++ WEB/WEB/Controllers/Home.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB.Controllers
+{
+    public class Home : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
