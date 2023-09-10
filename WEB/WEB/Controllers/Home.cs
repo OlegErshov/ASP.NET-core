cbaf16fed1d2fab0d44be4936635722b0a6bdeb5
Oleg Ershov <97069709+OlegErshov@users.noreply.github.com>
2023-09-10 19:58:29 +0300

    done second lab (#1)

--- WEB/WEB/Controllers/Home.cs
+++ WEB/WEB/Controllers/Home.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WEB.Models;
 
 namespace WEB.Controllers
 {
     public class Home : Controller
     {
+        [ViewData]
+        public string Name { get; set; }
         public IActionResult Index()
         {
-            return View();
+            Name = "Лабораторная работа номер 2";
+            List<DemoList> demoList = new List<DemoList> { new DemoList(1,"Oleg"), new DemoList(2,"Tima"),
+            new DemoList(3,"Sergey")};
+            return View(demoList);
         }
     }
+
 }
---
+++ WEB/WEB/Models/DemoList.cs
@@ -0,0 +1,15 @@
+﻿namespace WEB.Models
+{
+    public class DemoList
+    {
+        public DemoList(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+}
