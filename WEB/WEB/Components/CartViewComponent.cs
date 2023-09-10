2fd4e5ae9ab9226a2eecf92420cf630f798cee1b
olezh <olezhka1629@gmail.com>
2023-09-10 19:56:39 +0300

    done second lab

---
+++ WEB/WEB/Components/CartViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB.Components
+{
+    [ViewComponent]
+    public class CartViewComponent 
+    {
+        public string Invoke(double price)
+        {
+            return price.ToString();
+        }
+    }
+}
